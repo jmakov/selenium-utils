1f056f966c1c45206c648c38c52e5b1167e0fc97
Jernej Makovsek <jernej.makovsek@gmail.com>
2017-03-22 20:41:35 +0100

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Common utility functions when dealing with Selenium Webdriver.
+#Common utility functions when dealing with Selenium Webdriver.
+
+## Compatibility
+Because of type hinting is extensively used, only Python 3.5 and above are supported.
--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache 2.0',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
